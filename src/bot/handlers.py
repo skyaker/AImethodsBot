f74ca38820cd702a055a0996be2af0cfacd61e2a
Jung Dong Wook <sasha.chun.2003@mail.ru>
2024-11-17 01:25:40 +0300

    added: text analyzing mode fixed: keyboard buttons for each page

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,28 +1,55 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from src.bot.bot import bot
 from src.model.model import SentimentModel
-from src.bot.keyboards import get_main_keyboard
+from src.bot.keyboards import *
 
 router = Router()
 
 sentiment_model = SentimentModel()
 
+class TextAnalysisState(StatesGroup):
+  analyzing = State()
+
+async def is_not_button(message: types.Message) -> bool:
+  return message.text.lower() not in ["Анализировать текст", "Помощь"]
+
+
 @router.message(Command(commands=["start"]))
 async def start_command(message: types.Message):
-    """Command handler /start"""
-    keyboard = get_main_keyboard()
-    await message.reply("Привет! Я бот для анализа тональности текста. Напишите любое сообщение, и я проанализирую его", reply_markup=keyboard)
+  """Command handler /start"""
+  keyboard = get_main_keyboard()
+  await message.reply("Привет! Я бот для анализа тональности текста. Чем могу помочь?", reply_markup=keyboard)
 
 
 @router.message(Command(commands=["help"]))
 async def help_command(message: types.Message):
-    """Command handler /help"""
-    await message.reply("Необходимо отправить текстовое сообщение в чат, после чего я напишу результат анализа")
+  """Command handler /help"""
+  await message.reply("Необходимо отправить текстовое сообщение в чат, после чего я напишу результат анализа")
 
 
-@router.message()
-async def analyze_message(message: types.Message):
+@router.message(F.text == "Анализировать текст")
+async def enter_analysis_mode(message: types.Message, state: FSMContext):
+  await message.reply("Теперь вы можете отправить текст для анализа.", reply_markup=get_analyze_keyboard())
+  await state.set_state(TextAnalysisState.analyzing)
+
+
+@router.message(F.text == "Помощь")
+async def help_button(message: types.Message):
+  """Button for help"""
+  await help_command(message)
+
+
+@router.message(F.text == "Выйти в главное меню")
+async def exit_analysis_mode(message: types.Message, state: FSMContext):
+  await message.reply("Вы вышли в главное меню.", reply_markup=get_main_keyboard())
+  await state.clear()
+
+
+@router.message(TextAnalysisState.analyzing)
+async def analyze_text(message: types.Message, state: FSMContext):
   """Message analyze handler"""
   try:
     result = sentiment_model.analyze_sentiment(message.text)
@@ -31,18 +58,6 @@ async def analyze_message(message: types.Message):
     await message.reply(f"Тональность: {label}\nУверенность: {score:.2f}")
   except Exception as e:
     await message.reply(f"Ошибка: {str(e)}")
-
-
-@router.message(lambda message: message.text.lower() == "анализировать текст")
-async def analyze_button(message: types.Message):
-  """Button for analyzer"""
-  await message.reply("Напишите текст, который хотите проанализировать")
-
-
-@router.message(lambda message: message.text.lower() == "помощь")
-async def help_button(message: types.Message):
-  """Button for help"""
-  await help_command(message)
 
 
 def register_handlers(dp):
--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -1,5 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# набор кнопок под сценарий
 def get_main_keyboard():
   """Main bot keyboard"""
   keyboard = [
@@ -7,3 +8,11 @@ def get_main_keyboard():
     [KeyboardButton(text="Помощь")]
   ]
   return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+def get_analyze_keyboard():
+  """Analyze page keyboard"""
+  keyboard = [
+    [KeyboardButton(text = "Выйти в главное меню")]
+  ]
+  keyboard = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+  return keyboard
